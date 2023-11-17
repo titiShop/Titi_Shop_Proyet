a41d21323ce07f3707b971af1ce0c6013e172906
aleiser <130762145+yostin321@users.noreply.github.com>
2023-11-17 09:54:38 -0500

    Productos
    Se creo el archivo productos y productos dao

---
+++ src/Modelo/ProductosDao.java
@@ -0,0 +1,16 @@
+
+package Modelo;
+
+import com.mysql.cj.jdbc.PreparedStatementWrapper;
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author aleiz
+ */
+public class ProductosDao {
+    Connection con ;
+    Conexion cn = new Conexion();
+    PreparedStatemen ps ;
+    
+}
