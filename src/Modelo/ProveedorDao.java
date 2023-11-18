72ba0525c074fa5482cb04349eac0942b2336e92
crsitina IP <130761746+titi182@users.noreply.github.com>
2023-11-17 21:24:33 -0500

    Update ProveedorDao.java

--- src/Modelo/ProveedorDao.java
+++ src/Modelo/ProveedorDao.java
@@ -1,34 +1,36 @@
-
 package Modelo;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class ProveedorDao {  
+public class ProveedorDao {
+
     Connection con;
     Conexion cn = new Conexion();
     PreparedStatement ps;
-    public boolean RegistrarProveedor(Proveedor pr){
-      String sql = "INSERT INTO proveedor(nit, nombre, telefono, direccion, razon) VALUES (?,?,?,?,?)";
-      try {
-         con = cn.getConnection();
-         ps = con.prepareStatement(sql);
-         ps.setInt(1, pr.getNit());
-         ps.setString(2, pr.getNombre());
-         ps.setInt(3, pr.getTelefono());
-         ps.setString(4, pr.getDireccion());
-         ps.setString(5, pr.getRazon());
-         ps.execute();
-         return true;
-      } catch (SQLException e){
-          System.out.println(e.toString());
-          return false;
-      } finally{
-          try{
-              con.close();
-          } catch (SQLException e) {
-              System.out.println(e.toString());
-          }
-      }
-  }
+
+    public boolean RegistrarProveedor(Proveedor pr) {
+        String sql = "INSERT INTO proveedor(nit, nombre, telefono, direccion, razon) VALUES (?,?,?,?,?)";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pr.getNit());
+            ps.setString(2, pr.getNombre());
+            ps.setInt(3, pr.getTelefono());
+            ps.setString(4, pr.getDireccion());
+            ps.setString(5, pr.getRazon());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
