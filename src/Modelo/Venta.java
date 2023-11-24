e67972acbf8e06084960a288a212da2b69c58861
SantiSZA <santigosza@gmail.com>
2023-11-24 09:02:46 -0500

    AA

--- src/Modelo/Venta.java
+++ src/Modelo/Venta.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
-/**
- *
- * @author CEDENORTE
- */
+
 public class Venta {
-    
+     private int id;
+     private String cliente;
+     private String vendedor;
+     private double total;
+     
+    public Venta(){
+        
+    }
+
+    public Venta(int id, String cliente, String vendedor, double total) {
+        this.id = id;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.total = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+     
+     
 }
